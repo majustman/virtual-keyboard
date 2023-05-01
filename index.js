8fbd159b10fe0fd9b2e02c38f3c5696e76753494
majustman <samatt90@gmail.com>
2023-05-02 00:02:16 +0600

    feat: add info text

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import * as textArea from './components/text-area/text-area.js';
 import * as KeyBoard from './components/keyboard/keyboard.js';
+import createElement from './extensions/create-element.js';
 
-const main = document.createElement('main');
-main.classList.add('main');
+const header = createElement('header', 'header');
+const heading = createElement('h1', 'heading');
+heading.textContent = 'RSS Virtual Keyboard';
+header.append(heading);
+document.body.append(header);
+
+const main = createElement('main', 'main');
 main.append(textArea.createComponent());
 main.append(KeyBoard.createComponent());
+const info = createElement('div', 'info');
+const os = createElement('p', 'os');
+os.textContent = 'Keyboard has been made in Ubuntu';
+info.append(os);
+const lang = createElement('p', 'lang');
+lang.textContent = 'Use left Alt + left Shift for language shift';
+info.append(lang);
+main.append(info);
 document.body.append(main);
 
 document.addEventListener('keydown', KeyBoard.KeyDownHandler);
